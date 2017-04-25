a2987698eb0a8fdf91793778340bbc30c12b77a5
Roshan Choxi <roshan.choxi@gmail.com>
2017-04-24 20:32:18 -0500

    add HTML outline

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React from 'react'
 
 export default class App extends React.Component {
   render() {
-    return (<div>
-      <h2>Welcome to React!</h2>
-    </div>);
+    return (
+      <div>
+        <h1>Trellis</h1>
+        <div class="List">
+          <div class="List__title"> Icebox </div>
+          <div class="Card">
+            <div class="Card__title"> Rewrite everything in Crystal </div>
+          </div>
+          <div class="Card">
+            <div class="Card__title"> Synergize </div>
+          </div>
+          <div class="Card">
+            <div class="Card__title"> Solve AGI </div>
+          </div>
+        </div>
+
+        <div class="List">
+          <div class="List__title"> Active </div>
+          <div class="Card">
+            <div class="Card__title"> Add more "pop" to the landing page </div>
+          </div>
+        </div>
+
+        <div class="List">
+          <div class="List__title"> Done </div>
+          <div class="Card">
+            <div class="Card__title"> Rewrite everything in Go </div>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
