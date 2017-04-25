b862b6f87f93229e0f10bf3f193f387a3077ec7b
Roshan Choxi <roshan.choxi@gmail.com>
2017-04-25 14:43:15 -0500

    make cards draggable

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -2,13 +2,22 @@ import React from 'react'
 import ListCard from './list_card'
 
 export default class List extends React.Component {
+  onDrop(event) {
+    let cardTitle = event.dataTransfer.getData("text")
+    console.log(cardTitle)
+  }
+
+  preventDefault(event) {
+    event.preventDefault()
+  }
+
   render() {
     let listCards = this.props.cards.map((card) => {
       return <ListCard key={ card.title } title={ card.title }/>
     })
 
     return (
-      <div className="List">
+      <div className="List" onDrop={ this.onDrop } onDragOver={ this.preventDefault } >
         <div className="List__title"> Icebox </div>
         { listCards }
         <div className="AddCard">
--- src/components/list_card.jsx
+++ src/components/list_card.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
 export default class ListCard extends React.Component {
+  constructor() {
+    super()
+
+    // Hard bind `onDragStart` to this object so we can
+    // access `this.props` in the event handler
+    this.onDragStart = this.onDragStart.bind(this)
+  }
+
+  onDragStart(event) {
+    event.dataTransfer.effectAllowed = 'move'
+    event.dataTransfer.setData("text", this.props.title)
+  }
+
   render() {
     return (
-      <div className="ListCard">
+      <div
+        className="ListCard"
+        draggable="true"
+        onDragStart={ this.onDragStart } >
         <div className="ListCard__title"> { this.props.title }</div>
       </div>
     )
