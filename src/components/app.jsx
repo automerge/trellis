27504dc743b09264279d84d5b0e236ffe5850bad
Roshan Choxi <roshan.choxi@gmail.com>
2017-04-25 09:29:21 -0500

    rename Card component to ListCard

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -9,19 +9,19 @@ export default class App extends React.Component {
         <h1>Trellis</h1>
         <div className="List">
           <div className="List__title"> Icebox </div>
-          <div className="Card">
-            <div className="Card__title"> Rewrite everything in Crystal </div>
+          <div className="ListCard">
+            <div className="ListCard__title"> Rewrite everything in Crystal </div>
             <div className="Members">
               <div className="Members__member">
                 <img src={gravatarURL} />
               </div>
             </div>
           </div>
-          <div className="Card">
-            <div className="Card__title"> Synergize </div>
+          <div className="ListCard">
+            <div className="ListCard__title"> Synergize </div>
           </div>
-          <div className="Card">
-            <div className="Card__title"> Solve AGI </div>
+          <div className="ListCard">
+            <div className="ListCard__title"> Solve AGI </div>
           </div>
           <div className="AddCard">
             <a href="#">Add a card...</a>
@@ -30,8 +30,8 @@ export default class App extends React.Component {
 
         <div className="List">
           <div className="List__title"> Active </div>
-          <div className="Card">
-            <div className="Card__title"> Add more "pop" to the landing page </div>
+          <div className="ListCard">
+            <div className="ListCard__title"> Add more "pop" to the landing page </div>
           </div>
           <div className="AddCard">
             <a href="#">Add a card...</a>
@@ -40,8 +40,8 @@ export default class App extends React.Component {
 
         <div className="List">
           <div className="List__title"> Done </div>
-          <div className="Card">
-            <div className="Card__title"> Rewrite everything in Go </div>
+          <div className="ListCard">
+            <div className="ListCard__title"> Rewrite everything in Go </div>
           </div>
           <div className="AddCard">
             <a href="#">Add a card...</a>
