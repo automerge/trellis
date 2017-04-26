91d5f427deceb264d5187bf52f460eb908a59fa3
Roshan Choxi <roshan.choxi@gmail.com>
2017-04-25 19:45:55 -0500

    move initial state into json file

--- src/store.js
+++ src/store.js
@@ -2,44 +2,7 @@ import { createStore } from 'redux'
 
 export default class Store {
   constructor() {
-    let initialState = {}
-    initialState.lists = [
-      {
-        id: 1,
-        title: "icebox"
-      },
-      {
-        id: 2,
-        title: "active"
-      },
-      {
-        id: 3,
-        title: "done"
-      }
-    ]
-
-    initialState.cards = [
-      {
-        id: 1,
-        listId: 1,
-        title: "Rewrite everything in Crystal"
-      },
-      {
-        id: 2,
-        listId: 1,
-        title: "Solve AGI"
-      },
-      {
-        id: 3,
-        listId: 2,
-        title: "Add more 'pop' to the landing page"
-      },
-      {
-        id: 4,
-        listId: 3,
-        title: "Rewrite everything in Go"
-      }
-    ]
+    let initialState = require("../initial_state.json") 
 
     this.reduxStore = createStore((state = initialState, action) => {
       switch(action.type) {
