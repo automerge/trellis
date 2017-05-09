a3187eacd0ff226d23d4f2f6b1fda93734c37d5b
Adam Wiggins <a@adamwiggins.com>
2017-05-09 11:26:09 +0200

    inspector lists cards in a table view

--- src/components/inspector.jsx
+++ src/components/inspector.jsx
@@ -6,6 +6,16 @@ export default class Inspector extends React.Component {
   }
 
   render() {
-    return <div className="Inspector">{ JSON.stringify(this.props.store.getState()) }</div>
+    let listCardsPartial = this.props.store.getState().cards.map((card) => {
+      return <tr><td>{card.id}</td><td>{card.listId}</td><td>{card.title}</td></tr>
+    })
+
+    return <div className="Inspector">
+      <h3>Cards</h3>
+      <table>
+        <tr><th>id</th><th>listId</th><th>title</th></tr>
+        { listCardsPartial }
+      </table>
+    </div>
   }
 }
