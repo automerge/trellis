4da4e3de3cbb2ce447bf55f15fbfedef19c92fc6
Adam Wiggins <a@adamwiggins.com>
2017-05-09 12:02:36 +0200

    input box for changing listId from the Inspector

--- src/components/inspector.jsx
+++ src/components/inspector.jsx
@@ -3,19 +3,38 @@ import React from 'react'
 export default class Inspector extends React.Component {
   constructor(props) {
     super(props)
+    this.updateListId = this.updateListId.bind(this)
+  }
+
+  updateListId(event) {
+    let cardId  = parseInt(event.target.name.replace(/[^0-9]/g, ''))
+    let card    = this.props.store.findCard(cardId)
+
+    let newListId = parseInt(event.target.value)
+    if (newListId >= 1 && newListId <= 3) {
+      card.listId = newListId
+      this.props.store.updateCard(card)
+    }
   }
 
   render() {
     let listCardsPartial = this.props.store.getState().cards.map((card) => {
-      return <tr><td>{card.id}</td><td>{card.listId}</td><td>{card.title}</td></tr>
+      let name = 'cardId[' + card.id + ']'
+      return <tr>
+        <td>{card.id}</td>
+        <td><input type="text" name={name} value={card.listId} onChange={this.updateListId} /></td>
+        <td>{card.title}</td>
+      </tr>
     })
 
     return <div className="Inspector">
       <h3>Cards</h3>
+      <form>
       <table>
-        <tr><th>id</th><th>listId</th><th>title</th></tr>
-        { listCardsPartial }
+        <thead><tr><th>id</th><th>listId</th><th>title</th></tr></thead>
+        <tbody>{ listCardsPartial }</tbody>
       </table>
+      </form>
     </div>
   }
 }
