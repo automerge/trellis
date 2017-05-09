21aaf0e29a03d3867ade19ca417f5e70197d62ca
Adam Wiggins <a@adamwiggins.com>
2017-05-09 12:09:48 +0200

    Inspector can edit card title

--- src/components/inspector.jsx
+++ src/components/inspector.jsx
@@ -4,6 +4,7 @@ export default class Inspector extends React.Component {
   constructor(props) {
     super(props)
     this.updateListId = this.updateListId.bind(this)
+    this.updateTitle = this.updateTitle.bind(this)
   }
 
   updateListId(event) {
@@ -17,13 +18,23 @@ export default class Inspector extends React.Component {
     }
   }
 
+  updateTitle(event) {
+    let cardId  = parseInt(event.target.name.replace(/[^0-9]/g, ''))
+    let card    = this.props.store.findCard(cardId)
+
+    let newTitle = event.target.value
+    card.title = newTitle
+    this.props.store.updateCard(card)
+  }
+
   render() {
     let listCardsPartial = this.props.store.getState().cards.map((card) => {
-      let name = 'cardId[' + card.id + ']'
+      let name1 = 'cardListId[' + card.id + ']'
+      let name2 = 'cardTitle[' + card.id + ']'
       return <tr>
         <td>{card.id}</td>
-        <td><input type="text" name={name} value={card.listId} onChange={this.updateListId} /></td>
-        <td>{card.title}</td>
+        <td><input type="text" className="number" name={name1} value={card.listId} onChange={this.updateListId} /></td>
+        <td><input type="text" className="string" name={name2} value={card.title} onChange={this.updateTitle} /></td>
       </tr>
     })
 
@@ -31,7 +42,7 @@ export default class Inspector extends React.Component {
       <h3>Cards</h3>
       <form>
       <table>
-        <thead><tr><th>id</th><th>listId</th><th>title</th></tr></thead>
+        <thead><tr><th>id</th><th>list</th><th>title</th></tr></thead>
         <tbody>{ listCardsPartial }</tbody>
       </table>
       </form>
