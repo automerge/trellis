b0a0c0a6b7ade9b90d5f519fd1cfec56e8f9f20e
Adam Wiggins <a@adamwiggins.com>
2017-05-10 12:48:27 +0200

    instantiate store inside App

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 import Board from './board'
 import Inspector from './inspector'
 import Connection from './connection'
+import Store from '../lib/store'
 
 export default class App extends React.Component {
   constructor(props) {
     super(props)
+    this.store = new Store()
   }
 
   render() {
     return (
       <div className="App">
-        <Board store={ this.props.store } />
-        <Inspector store={ this.props.store } />
+        <Board store={ this.store } />
+        <Inspector store={ this.store } />
         <Connection connected={true} />
       </div>
     )
