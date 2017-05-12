293d1bf904feef35f352d89bd189753fa403c45d
Adam Wiggins <a@adamwiggins.com>
2017-05-12 10:21:13 +0200

    all five team avatars

--- src/components/assignments.jsx
+++ src/components/assignments.jsx
@@ -8,8 +8,11 @@ export default class Assignments extends React.Component {
   render() {
     return (
       <div className="Assignments">
+        <img src="assets/images/avatars/adam.png" />
+        <img src="assets/images/avatars/orion.png" />
+        <img src="assets/images/avatars/pvh.png" />
         <img src="assets/images/avatars/roshan.png" />
-        <img src="assets/images/avatars/roshan.png" />
+        <img src="assets/images/avatars/martin.png" />
       </div>
     )
   }
