239456c12cb4d2957a54251cd2fbbcb766ab13c8
Adam Wiggins <a@adamwiggins.com>
2017-05-12 14:27:13 +0200

    toggle assignments in UI, no persistence

--- src/components/assignments.jsx
+++ src/components/assignments.jsx
@@ -3,17 +3,33 @@ import React from 'react'
 export default class Assignments extends React.Component {
   constructor() {
     super()
+    this.state = { assigned: new Set() }
+    this.toggle = this.toggle.bind(this)
+  }
+
+  toggle(event) {
+    let person = event.target.name
+    let assigned = this.state.assigned
+    if (assigned.has(person))
+      assigned.delete(person)
+    else
+      assigned.add(person)
+
+    this.setState({ assigned: assigned })
+  }
+
+  people() {
+    return [ 'adam', 'orion', 'pvh', 'roshan', 'martin' ]
   }
 
   render() {
+    let assignments = this.people().map((person) => {
+      let fname = "assets/images/avatars/" + person + ".png"
+      let klass = this.state.assigned.has(person) ? "Assignments__active" : "Assignments__inactive"
+      return <img key={person} name={person} src={fname} className={klass} onClick={ this.toggle } />
+    })
     return (
-      <div className="Assignments">
-        <img src="assets/images/avatars/adam.png" />
-        <img src="assets/images/avatars/orion.png" />
-        <img src="assets/images/avatars/pvh.png" />
-        <img src="assets/images/avatars/roshan.png" />
-        <img src="assets/images/avatars/martin.png" />
-      </div>
+      <div className="Assignments">{assignments}</div>
     )
   }
 }
