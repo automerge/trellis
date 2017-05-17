9ec9d4ca720df3b22a9d1b184eb69614b0e0aed6
Roshan Choxi <roshan.choxi@gmail.com>
2017-05-16 20:24:19 -0500

    make inspector read-only

--- src/components/inspector.jsx
+++ src/components/inspector.jsx
@@ -4,8 +4,6 @@ import TesseractInfo from './tesseract_info'
 export default class Inspector extends React.Component {
   constructor(props) {
     super(props)
-    this.updateListId = this.updateListId.bind(this)
-    this.updateTitle = this.updateTitle.bind(this)
 
     this.store = this.props.store
 
@@ -13,22 +11,8 @@ export default class Inspector extends React.Component {
     this.store.subscribe((x) => { this.setState(this.store.getState()) })
   }
 
-  updateListId(event) {
-    let index = parseInt(event.target.name.replace(/[^0-9]/g, ''))
-    let newListId = parseInt(event.target.value)
-
-    if (newListId >= 1 && newListId <= 3) {
-      this.store.root.cards[index].listId = newListId
-      this.setState(this.store.getState())
-    }
-  }
-
-  updateTitle(event) {
-    let index = parseInt(event.target.name.replace(/[^0-9]/g, ''))
-    let newTitle = event.target.value
-
-    this.store.root.cards[index].title = newTitle
-    this.setState(this.store.getState())
+  formatUUID(uuid) {
+    return uuid.toUpperCase().substring(0,4)
   }
 
   render() {
@@ -39,9 +23,9 @@ export default class Inspector extends React.Component {
         let name1 = 'cardListId[' + index + ']'
         let name2 = 'cardTitle[' + index + ']'
         return <tr key={index}>
-          <td>{card.id}</td>
-          <td><input type="text" className="number" name={name1} value={card.listId} onChange={this.updateListId} /></td>
-          <td><input type="text" className="string" name={name2} value={card.title} onChange={this.updateTitle} /></td>
+          <td>{this.formatUUID(card.id)}… </td>
+          <td>{this.formatUUID(card.listId)}… </td>
+          <td>{card.title}</td>
         </tr>
       })
     }
