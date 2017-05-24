1f2dbbb47baea3cb8286ab00d0596ed4270d2533
Adam Wiggins <a@adamwiggins.com>
2017-05-24 20:26:31 +0200

    peers mockup

--- src/components/peers.jsx
+++ src/components/peers.jsx
@@ -8,6 +8,24 @@ export default class Peers extends React.Component {
   render() {
     return <div className="Peers">
       <h2>Peers <img src="assets/images/peers.svg" /></h2>
+      <table>
+        <tbody>
+          <tr>
+            <td className="LED"><img src="assets/images/LED-green.svg" /></td>
+            <td className="user">Adam</td>
+            <td className="device">iMac</td>
+            <td className="ip">204.1.73.21</td>
+            <td className="lastChange">3 seconds ago</td>
+          </tr>
+          <tr>
+            <td className="LED"><img src="assets/images/LED-yellow.svg" /></td>
+            <td className="user">Adam</td>
+            <td className="device">Surface</td>
+            <td className="ip">204.1.73.9</td>
+            <td className="lastChange">20 minutes ago</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   }
 }
