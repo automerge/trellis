31a80043513f933d362e7ab20184dc3ccc950c3c
Adam Wiggins <a@adamwiggins.com>
2017-05-30 17:43:23 +0300

    changes mostly static list based on state history

--- src/components/changes.jsx
+++ src/components/changes.jsx
@@ -3,11 +3,20 @@ import React from 'react'
 export default class Changes extends React.Component {
   constructor(props) {
     super(props)
+
+    this.store = this.props.store
+    this.state = this.store.getState()
+    this.store.subscribe((x) => { this.setState(this.store.getState()) })
   }
 
   render() {
+    let changesPartial = this.store.stateHistory.map((change) => {
+      return <li>Martin created a card</li>
+    })
+
     return <div className="Changes">
       <h2>Changes <img src="assets/images/delta.svg" /></h2>
+      <ul>{changesPartial}</ul>
     </div>
   }
 }
