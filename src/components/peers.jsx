9786e32997872e760928b70f5542eebcabbed38f
Peter van Hardenberg <pvh@pvh.ca>
2017-05-31 16:21:02 -0700

    a terrible peer clock. sorry for my shit formatting, adam

--- src/components/peers.jsx
+++ src/components/peers.jsx
@@ -30,6 +30,29 @@ export default class Peers extends React.Component {
         this.props.network.disconnect()
   }
 
+  formatUUID(uuid) {
+    return uuid.toUpperCase().substring(0,2) + "…"
+  }
+
+  formatVectorClock(clock) {
+    let heads = Object.keys(clock).map( (peer_id) => {
+        return <th className="peerID"> { this.formatUUID(peer_id) } </th>
+    })
+    let tails = Object.keys(clock).map( (peer_id) => {
+        return <td className="clockPosition"> { clock[peer_id] } </td>
+    })
+    return <div >
+      <table className="vectorClock">
+      <thead>
+          <tr>{heads}</tr>
+      </thead>
+      <tbody>
+        <tr>{tails}</tr>
+      </tbody>
+    </table>
+    </div>
+  }
+
   render() {
     let peers = this.state.peers
     let peersPartial = Object.keys(peers).map((id, index) => {
@@ -38,12 +61,15 @@ export default class Peers extends React.Component {
       let ledColor = peer.connected ? "green" : "yellow"
       let ledPath = "assets/images/LED-" + ledColor + ".svg"
       let key = "peer-" + id
+      
+      let clock = this.props.network.clocks[id];
 
       return <tr key={key}>
             <td className="LED"><img src={ledPath} /></td>
             <td className="user">{name}</td>
             <td className="activity">{index > 0 ? peer.messagesSent : ""}</td>
             <td className="activity">{index > 0 ? peer.messagesReceived : ""}</td>
+            <td className="clock">{clock ? this.formatVectorClock(clock) : "no clock"}</td>
           </tr>
     })
 
