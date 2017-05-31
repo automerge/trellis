599ea43db78ded40074629e5c78e24f959e6d54a
Roshan Choxi <roshan.choxi@gmail.com>
2017-05-31 09:11:42 -0500

    create DropTarget component

---
+++ src/components/drop_target.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default class DropTarget extends React.Component {
+  constructor() {
+    super()
+    this.onDrop = this.onDrop.bind(this)
+  }
+
+  preventDefault(event) {
+    event.preventDefault()
+  }
+ 
+  onDrop(event) {
+    let cardId = event.dataTransfer.getData("text")
+
+    this.props.store.dispatch({
+      type: "MOVE_CARD",
+      cardId: cardId,
+      listId: this.props.listId
+    })
+  }
+
+
+ render() {
+    // Chrome has a drag-and-drop bug that requires onDragOver to not propogate its event
+   return <div className="DropTarget" onDrop={ this.onDrop } onDragOver={ this.preventDefault } >
+   </div>
+ }
+}
