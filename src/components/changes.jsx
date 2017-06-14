1106c132e3f0edb2bb7a2e657c27284f17a20d76
Adam Wiggins <a@adamwiggins.com>
2017-06-14 19:15:32 +0200

    static mockup of change graph

--- src/components/changes.jsx
+++ src/components/changes.jsx
@@ -10,8 +10,28 @@ export default class Changes extends React.Component {
   }
 
   render() {
+    let changes = [ // standin data, replace with Tesseract.getHistory()
+      { id: 1, user: "Adam", action: "moved a card" },
+      { id: 2, user: "Roshan", action: "deleted a list" }
+    ]
+
+    let changesPartial = changes.map((change, index) => {
+      let key = "change-" + change.id
+
+      let edgeImg = ""
+      if (index < changes.length-1)
+        edgeImg = <img className="changeEdge" src="assets/images/change-edge.svg" />
+
+      return <li key={key}>
+        <img className="changeNode" src="assets/images/change-node.svg" />
+        {edgeImg}
+        {change.user} {change.action}
+      </li>
+    })
+
     return <div className="Changes">
       <h2>Changes <img src="assets/images/delta.svg" /></h2>
+      <ul>{changesPartial}</ul>
     </div>
   }
 }
