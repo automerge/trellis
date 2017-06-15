defe660ffd4503d922b67ef82e7afcd2bd159399
Adam Wiggins <a@adamwiggins.com>
2017-06-15 15:30:16 +0200

    move displayChange into react component so that we can build HTML

--- src/components/changes.jsx
+++ src/components/changes.jsx
@@ -9,6 +9,45 @@ export default class Changes extends React.Component {
     this.store.subscribe((x) => { this.setState(this.store.getState()) })
   }
 
+  displayChange(change, prevChange) {
+    let meta = change.changeset.message
+    if (!meta || !meta.author || !meta.action) return ""
+
+    switch(meta.action.type) {
+      case "CREATE_CARD":
+        return meta.author + " created a card"
+      case "MOVE_CARD":
+        var prevCard = this.store.findCardFromState(meta.action.cardId, prevChange.snapshot)
+        var newCard = this.store.findCardFromState(meta.action.cardId, change.snapshot)
+        var prevList = this.store.findListFromState(prevCard.listId, prevChange.snapshot)
+        var newList = this.store.findListFromState(newCard.listId, change.snapshot)
+        return meta.author + " moved “" + newCard.title + "”" + " from “" + prevList.title + "”" + " to “" + newList.title + "”"
+      case "UPDATE_CARD_TITLE":
+        var newCard = this.store.findCardFromState(meta.action.cardId, change.snapshot)
+        return meta.author + " renamed card to “" + newCard.title + "”"
+      case "UPDATE_CARD_DESCRIPTION":
+        var newCard = this.store.findCardFromState(meta.action.cardId, change.snapshot)
+        return meta.author + " changed description of “" + newCard.title + "”"
+      case "DELETE_CARD":
+        var card = this.store.findCardFromState(meta.action.cardId, prevChange.snapshot)
+        return meta.author + " deleted “" + card.title + "”"
+      case "UPDATE_ASSIGNMENTS":
+        var prevCard = this.store.findCardFromState(meta.action.cardId, prevChange.snapshot)
+        var newCard = this.store.findCardFromState(meta.action.cardId, change.snapshot)
+        if (Object.keys(newCard.assigned) > Object.keys(prevCard.assigned))
+          return meta.author + " assigned “" + newCard.title + "”"
+        else
+          return meta.author + " unassigned “" + newCard.title + "”"
+      case "CREATE_LIST":
+        return meta.author + " created a list"
+      case "DELETE_LIST":
+        var list = this.store.findListFromState(meta.action.listId, prevChange.snapshot)
+        return meta.author + " deleted list “" + list.title + "”"
+      default:
+        return meta.author + " " + meta.action.type
+    }
+  }
+
   render() {
     const changesToShow = 10
     let changes = this.store.getHistory().slice((changesToShow+1) * -1)
@@ -22,7 +61,7 @@ export default class Changes extends React.Component {
       if (index < changes.length-1)
         edgeImg = <div className="changeEdge" />
 
-      let changeMessage = this.store.displayChange(change, prevChange)
+      let changeMessage = this.displayChange(change, prevChange)
       prevChange = change
 
       let klass = ""
