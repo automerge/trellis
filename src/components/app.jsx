a5fd0f64698af7ab419f31fee8f7ae1f7c28fe8d
Adam Wiggins <a@adamwiggins.com>
2017-06-15 19:43:11 +0200

    remove unused highlight interface

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -21,7 +21,6 @@ export default class App extends React.Component {
     window.app    = this
     this.autoSave = this.autoSave.bind(this)
     this.store    = new Store()
-    this.state    = { highlightOptions: null }
 
     this.store.subscribe(() => {
       this.setState({}) // Force component to re-render
@@ -46,11 +45,6 @@ export default class App extends React.Component {
     ipcRenderer.on("shareToClipboard", (event) => {
       ipcRenderer.send("shareToClipboardResult", this.getDocUrl())
     })
-  }
-
-  // Ex. this.highlight({ cardId: this.store.getState().cards[0].id })
-  highlight(options) {
-    this.setState({ highlightOptions: options })
   }
 
   componentDidMount() {
