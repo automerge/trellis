eae787a0fa09f1536e29b563684ce54dd91a1347
Adam Wiggins <a@adamwiggins.com>
2017-06-21 19:41:38 +0200

    four document mockup cards

--- src/components/documents.jsx
+++ src/components/documents.jsx
@@ -6,13 +6,24 @@ export default class Documents extends React.Component {
   }
 
   render() {
+    let documents = [
+      { docid: 'gray-istanbul-62', title: 'Sprint Planning', users: 'Alice・Bob' },
+      { docid: 'cobalt-jakarta-40', title: 'Untitled', users: 'Carlos' },
+      { docid: 'emerald-tokyo-57', title: 'Avengers Recruiting', users: '' },
+      { docid: 'cobalt-delhi-85', title: 'Sprint Planning Fork', users: '' },
+    ]
+
+    let documentsPartial = documents.map((doc) => {
+      return <div key={doc.docid} className="document">
+        <div className="docid">{doc.docid}</div>
+        <div className="title">{doc.title}</div>
+        <div className="users">{doc.users}</div>
+      </div>
+    })
+
     return <div className="Documents">
       <h2>Documents <img src="assets/images/documents.svg" /></h2>
-      <div className="document">
-        <div className="docid">gray-istanbul-62</div>
-        <div className="title">Sprint Planning</div>
-        <div className="users">Alice ・ Bob</div>
-      </div>
+      {documentsPartial}
     </div>
   }
 }
