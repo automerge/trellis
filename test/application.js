2e3d6da4196df8fd8b5d4e75e80ba5cf635ee4f0
Roshan Choxi <roshan.choxi@gmail.com>
2017-06-21 12:45:19 -0500

    fix edit card test

--- test/application.js
+++ test/application.js
@@ -99,7 +99,7 @@ describe('application', function () {
     .then(() => this.app.client.click(".ListCard__title"))
     .then(() => this.app.client.click(".Modal .InlineInput div"))
     .then(() => this.app.client.setValue(".InlineInput textarea", "New Title"))
-    .then(() => this.app.client.keys(["Shift", "Enter"]))
+    .then(() => this.app.client.keys(["Enter"]))
     .then(() => this.app.client.getText(".ListCard__title"))
     .then((title) => assert.equal(title, "New Title") )
   })
